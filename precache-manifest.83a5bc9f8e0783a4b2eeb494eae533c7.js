7c337b5dd5f4977e71a30d061d019b108e33d7c6
Alisson Oliveira <git@alisson.dev>
2020-02-09 15:41:24 -0300

    Updates

---
+++ precache-manifest.83a5bc9f8e0783a4b2eeb494eae533c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f937fd49396b31dcdeaf21f533d7fa36",
+    "url": "/just-a-dance/index.html"
+  },
+  {
+    "revision": "42e352031d431b8cdfae",
+    "url": "/just-a-dance/static/css/main.00662407.chunk.css"
+  },
+  {
+    "revision": "8ca778bac14048931a44",
+    "url": "/just-a-dance/static/js/2.ddd06a4b.chunk.js"
+  },
+  {
+    "revision": "a0e592d55698ec7f019963a03d5f862f",
+    "url": "/just-a-dance/static/js/2.ddd06a4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42e352031d431b8cdfae",
+    "url": "/just-a-dance/static/js/main.c230ecbf.chunk.js"
+  },
+  {
+    "revision": "707dfd2dc6ee746fb083",
+    "url": "/just-a-dance/static/js/runtime-main.ebe79e7a.js"
+  },
+  {
+    "revision": "f15bd30071ec3a42dd06d487e09fa0ed",
+    "url": "/just-a-dance/static/media/just-a-dance-logo.f15bd300.svg"
+  }
+]);
